e7bd2080dd8786b74cd34af95d0a61ab66a455fe
jalbarado <jalbarado741@gmail.com>
2021-06-02 19:41:07 -0500

    SVGR: de SVG a componente de ReactJS

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import {ListOfCategories} from './components/ListOfCategories';
 import {GlobalStyle} from './GlobalStyles';
 import {ListOfPhotoCards} from './components/ListOfPhotoCards';
+import {Logo} from './components/Logo';
 
 export const App = () => (
   <div>
     {/*GlobalStyle se agrega para que se apliquen los estilos globales*/}
     <GlobalStyle/>
+    <Logo/>
     <ListOfCategories/>
     <ListOfPhotoCards/>
   </div>
