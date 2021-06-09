efa29420151a8d89dcc43c5147a85ea821a36afc
jalbarado <jalbarado741@gmail.com>
2021-06-09 13:47:31 -0500

    custom hooks

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,48 +1,16 @@
-import React, {Fragment, useEffect, useRef, useState} from 'react';
+import React, {Fragment} from 'react';
 import {Button, Img, ImgWrapper} from './styles';
 import {MdFavorite, MdFavoriteBorder} from 'react-icons/md';
 import {Article} from '../ListOfCategories/styles';
+import {useLocalStorage} from '../../hooks/useLocalStorage';
+import {useNearScreen} from '../../hooks/useNearScreen';
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1500879747858-bb1845b61beb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60';
 
 export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
   const key = `like-${id}`
-  const element = useRef(null)
-  const [show, setShow] = useState(false)
-  const [liked, setLiked] = useState(() => {
-    try {
-      return window.localStorage.getItem(key)
-    } catch (e) {
-      return false
-    }
-  })
-
-  useEffect(() => {
-    // Se verifica que el objeto window contenga el feature IntersectionObserver, si no lo tiene lo carga del polyfill
-    // y lo devuelve en la promesa. En el caso de chrome no lo necesita, pero en el caso de Edge, si.
-    Promise.resolve(
-      typeof window.IntersectionObserver != 'undefined'
-        ? window.IntersectionObserver
-        : import('intersection-observer')).then(() => {
-      const observer = new window.IntersectionObserver((entries) => {
-        const {isIntersecting} = entries[0]
-        if (isIntersecting) {
-          setShow(true)
-          observer.disconnect()
-        }
-      })
-      observer.observe(element.current)
-    })
-  }, [element])
-
-  const setLocalStorage = value => {
-    try {
-      window.localStorage.setItem(key, value)
-      setLiked(value)
-    } catch (e) {
-      console.error(e)
-    }
-  }
+  const [liked, setLiked] = useLocalStorage(key, false)
+  const [show, element] = useNearScreen()
 
   const Icon = liked ? MdFavorite : MdFavoriteBorder
   return (
@@ -55,7 +23,7 @@ export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
               <Img src={src}/>
             </ImgWrapper>
           </a>
-          <Button onClick={() => setLocalStorage(!liked)}>
+          <Button onClick={() => setLiked(!liked)}>
             <Icon size="32px"/> {likes} likes!
           </Button>
         </Fragment>
---
+++ src/hooks/useNearScreen.js
@@ -0,0 +1,25 @@
+import {useEffect, useRef, useState} from 'react';
+
+export const useNearScreen = () => {
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
+  useEffect(() => {
+    // Se verifica que el objeto window contenga el feature IntersectionObserver, si no lo tiene lo carga del polyfill
+    // y lo devuelve en la promesa. En el caso de chrome no lo necesita, pero en el caso de Edge, si.
+    Promise.resolve(
+      typeof window.IntersectionObserver != 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')).then(() => {
+      const observer = new window.IntersectionObserver((entries) => {
+        const {isIntersecting} = entries[0]
+        if (isIntersecting) {
+          setShow(true)
+          observer.disconnect()
+        }
+      })
+      observer.observe(element.current)
+    })
+  }, [element])
+
+  return [show, element]
+}
