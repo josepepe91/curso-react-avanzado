613b9a0480c232314870480cb01c3f9c91a753cd
jalbarado <jalbarado741@gmail.com>
2021-06-09 16:48:10 -0500

    render props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {ListOfCategories} from './components/ListOfCategories';
 import {GlobalStyle} from './styles/GlobalStyles';
-import {ListOfPhotoCards} from './components/ListOfPhotoCards';
 import {Logo} from './components/Logo';
+import {ListOfPhotoCards} from './components/ListOfPhotoCards';
+import {PhotoCardWithQuery} from './containers/PhotoCardWithQuery';
 
-export const App = () => (
-  <div>
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+  console.log(detailId)
+
+  return (<div>
     {/*GlobalStyle se agrega para que se apliquen los estilos globales*/}
     <GlobalStyle/>
     <Logo/>
-    <ListOfCategories/>
-    <ListOfPhotoCards categoryId={1}/>
-  </div>
-)
+    {
+      detailId
+        ?
+        <PhotoCardWithQuery id={detailId}/>
+        :
+        <Fragment>
+          <ListOfCategories/>
+          <ListOfPhotoCards categoryId={1}/>
+        </Fragment>
+    }
+  </div>)
+}
---
+++ src/containers/PhotoCardWithQuery.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {PhotoCard} from '../components/PhotoCard';
+import {useGetSinglePhoto} from '../hooks/useGetSinglePhoto';
+
+export const PhotoCardWithQuery = ({id}) => {
+  const {loading, error, data} = useGetSinglePhoto(id)
+  if (error) {
+    return <h2>Internal Server Error</h2>
+  }
+  if (loading) {
+    return <h2>loading...</h2>
+  }
+
+  const {photo = {}} = data
+  return <PhotoCard {...photo}/>
+}
