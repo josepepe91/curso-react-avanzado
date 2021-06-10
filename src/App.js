e0777aa4848c8d9478a2c870edf8da0fade387b0
jalbarado <jalbarado741@gmail.com>
2021-06-10 12:41:15 -0500

    Rutas protegidas

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import {Logo} from './components/Logo';
 import {Home} from './pages/Home';
 import {Router} from '@reach/router';
 import {Detail} from './pages/Detail';
+import {Favs} from './pages/Favs';
+import {User} from './pages/User';
+import {NotRegisterUser} from './pages/NotRegisterUser';
 import {NavBar} from './components/NavBar';
+
+const UserLogged = ({children}) => {
+  return children({isAuth: false})
+}
 
 export const App = () => {
 
@@ -17,6 +24,21 @@ export const App = () => {
       <Home path="/pet/:categoryId"/>
       <Detail path="/detail/:detailId"/>
     </Router>
+    <UserLogged>
+      {
+        ({isAuth}) =>
+          isAuth ?
+            <Router>
+              <Favs path="/favs"/>
+              <User path="/user"/>
+            </Router>
+            :
+            <Router>
+              <NotRegisterUser path="/favs"/>
+              <NotRegisterUser path="/user"/>
+            </Router>
+      }
+    </UserLogged>
     <NavBar/>
   </div>)
 }
