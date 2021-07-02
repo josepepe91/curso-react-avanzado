13ccb36e91ec6ed9857d66400300103ad3c2306c
jalbarado <jalbarado741@gmail.com>
2021-07-01 20:02:07 -0500

    Últimos retoques a las rutas de nuestra aplicación

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { Home } from './pages/Home'
-import { Router } from '@reach/router'
+import { Redirect, Router } from '@reach/router'
 import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisterUser } from './pages/NotRegisterUser'
 import { NavBar } from './components/NavBar'
-import Context from './Context'
+import { Context } from './Context'
+import { NotFound } from './pages/NotFound'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
   return (
     <div>
       {/* GlobalStyle se agrega para que se apliquen los estilos globales */}
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisterUser path='/login' />}
+        {!isAuth && <Redirect from='/favs' to='/login' />}
+        {!isAuth && <Redirect from='/user' to='/login' />}
+        {isAuth && <Redirect from='/login' to='/' />}
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
-      <Context.Consumer>
-        {
-        ({ isAuth }) =>
-          isAuth
-            ? <Router>
-              <Favs path='/favs' />
-              <User path='/user' />
-            </Router>
-            : <Router>
-              <NotRegisterUser path='/favs' />
-              <NotRegisterUser path='/user' />
-              </Router>
-      }
-      </Context.Consumer>
       <NavBar />
     </div>
   )
---
+++ src/pages/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const NotFound = () => {
+  return <h2>Esta página no existe :(</h2>
+}
